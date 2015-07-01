eca84477974e4b1cebfd7270db3896bc9a03b92c
sebarigonatto <sebastianrigonatto@gmail.com>
2015-07-01 17:06:52 -0300

    Update reservas.php
    muestra eventos sin items

--- admin/models/reservas.php
+++ admin/models/reservas.php
@@ -40,11 +40,22 @@ class ReservaModelReservas extends JModelList
 				  INNER JOIN az94y_items ON az94y_eventos_items.items_id = az94y_items.id
 		  
 		 */
-		$query->select(array('a.*', 'b.*', 'c.*'))
+		/*$query->select(array('a.*', 'b.*', 'c.*'))
 			->from($db->quoteName('#__reserva_reserva', 'a'))
 			->join('INNER', $db->quoteName('#__reserva_evento', 'b') . ' ON (' . $db->quoteName('a.evento_id') . ' = ' . $db->quoteName('b.id') . ')')
 			->join('INNER', $db->quoteName('#__reserva_item', 'c') . ' ON (' . $db->quoteName('a.item_id') . ' = ' . $db->quoteName('c.id') . ')')
 			->order('a.evento_id');
-		return $query;
+		return $query;*/
+		
+		 $query->select(array('a.precio AS precio_total', 'a.id AS reserva_id', 
+                                'b.id AS evento_id', 'c.id AS item_id', 
+                                'b.titulo AS evento_titulo', 'b.descripcion AS evento_descripcion', 'b.inicio AS evento_inicio', 'b.fin AS evento_fin',
+                                'b.lugar AS lugar','b.tel AS tel','b.mail AS mail',
+                                'c.descripcion AS item_descripcion', 'c.img AS imagen', 'c.costo AS item_costo', 'c.nombre AS item_nombre'))
+            ->from($db->quoteName('#__reserva_evento', 'b'))
+            ->join('LEFT OUTER', $db->quoteName('#__reserva_reserva', 'a') . ' ON (' . $db->quoteName('a.evento_id') . ' = ' . $db->quoteName('b.id') . ')')
+            ->join('LEFT OUTER', $db->quoteName('#__reserva_item', 'c') . ' ON (' . $db->quoteName('a.item_id') . ' = ' . $db->quoteName('c.id') . ')');
+			$query->order('evento_inicio', 'evento_id');
+	return $query;
 	}
 }
